fa2bc10897af7ce344224f0ab5f3d871f10fd78a
Jorge Plaza <39694654+jorge-plaza@users.noreply.github.com>
2026-02-09 00:27:52 +0100

    otra vez 0

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -117,7 +117,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
     lv_obj_t *art = lv_img_create(widget->obj);
     lv_img_set_src(art, &gopher);  // puedes cambiar &gopher por &balloon o &mountain
-    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 100, 0);
+    lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
